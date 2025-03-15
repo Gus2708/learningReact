468546f8020b2785d01fb73cc1bc8598f632a90f
Scary Gamer <scarygamer08@gmail.com>
2025-03-14 21:36:05 -0400

    first upload

---
+++ projects/00-hola-mundo/src/app.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <article>
+            <header>
+                <img src="https://unavatar.io/midudev" alt="midudev" srcset="" />
+                <div>
+                    <strong>Miguel Angel Duran</strong>
+                    <span>@midudev</span>
+                </div>
+                <aside>
+                    <button>Seguir</button>
+                </aside>
+            </header>
+        </article>
+
+    );
+};
+
+export default App;
